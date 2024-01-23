9157e85306e92d26ae6235e97e4304cad5f35a80
umarabid123 <umarabid709@gmail.com>
2024-01-24 00:53:57 +0500

    chnages

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="m-0 p-0 box-border">
+      <header>
+<div className="bg-hero-pattern w-[100%] h-[100vh] bg-no-repeat bg-cover ">
+    
+</div>
+      </header>
+    </div>
+  );
+};
+export default Header;
