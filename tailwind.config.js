9dc67b005491ff11f34b2acca7b1be6ac53a2663
umarabid123 <umarabid709@gmail.com>
2024-01-25 00:52:39 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,12 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          'hero-pattern': "url('/images/bg-img.webp')",
+          'hero-pattern': "url('/images/bg-img-1.webp')",
       },
+      backgroundColor:{
+       primary:"rgba(0, 0, 0, 0.5)",
+       base:"rgba(109, 109, 110, 0.7)"
+      }
     },
   },
   plugins: [],
