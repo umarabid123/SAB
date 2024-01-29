ddad4c61971939a924ad704bc045b9df070510a1
zainali225 <alilohar20@gmail.com>
2024-01-29 11:33:47 +0500

    re structure

---
+++ pages/watch.js
@@ -0,0 +1,22 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+
+const WatchPage = () => {
+
+
+    return (
+        <div>
+            <h2>watch page</h2>
+            <h2>watch page</h2>
+            <h2>watch page</h2>
+            <h2>watch page</h2>
+            <h2>watch page</h2>
+
+        </div>
+    )
+}
+
+
+
+export default WatchPage;
