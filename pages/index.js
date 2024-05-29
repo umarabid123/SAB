a8e9e7f99799eb93618071f01d57e1295090547f
umarabid123 <umarabid709@gmail.com>
2024-05-29 13:46:03 +0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Card from "@/components/Card";
 import Header from "@/components/Header";
 import HomeText from "@/components/HomeText";
 
@@ -8,24 +9,24 @@ const HomePage = () => {
         <Header />
         <hr />
         <HomeText />
-        <div className="text-img lg:py-24 sm:px-11 lg:px-6 xl:px-[106px]">
-        <div className="logo flex items-center justify-between flex-wrap lg:flex-nowrap lg:pt-24 sm:px-11 lg:px-6 xl:px-[106px]">
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
-            <img src="/images/spotify-light.svg" alt="" className="" />
+        <div className="text-img py-24">
+        <div className="logo flex items-center justify-between flex-wrap lg:flex-nowrap sm:px-11 lg:px-6 xl:px-[70px]">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
+                <img src="/images/spotify-light.svg" alt="" className="" />
           </div>
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
             <img src="/images/amazon-light.svg" alt="" className="" />
           </div>
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
             <img src="/images/nasa.svg" alt="" className="" />
           </div>
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
             <img src="/images/netflix.svg" alt="" className="" />
           </div>
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
             <img src="/images/unity-light.svg" alt="" className="" />
           </div>
-          <div className="img w-[50%] sm:w-[30%] lg:w-[30%] flex justify-center p-6">
+          <div className="img w-[50%] sm:w-[30%] lg:w-[27%] flex justify-center p-6">
             <img
               src="/images/shutterstock-light.svg"
               alt=""
@@ -34,10 +35,11 @@ const HomePage = () => {
           </div>
         </div>
         <div className="txt mx-auto mt-8 flex justify-center">
-        <p className="text-sm lg:w-[30%] text-center">The world's best product teams trust MUI to deliver an unrivaled experience for both developers and users.</p>
+        <p className="text-sm lg:w-[30%] text-center text-gray-600">The world's best product teams trust MUI to deliver an unrivaled experience for both developers and users.</p>
         </div>
         </div>
         <hr />
+        <Card />
       </div>
     </>
   );
